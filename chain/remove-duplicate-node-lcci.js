4c616d450072d3d4a4159a7a927eab82ac9ab6d6
liuxing95 <1358974006@qq.com>
2020-06-01 14:42:09 +0800

    移除重复节点

--- chain/remove-duplicate-node-lcci.js
+++ chain/remove-duplicate-node-lcci.js
@@ -2,6 +2,23 @@
 
 // 输入：[1, 2, 3, 3, 2, 1]
 //  输出：[1, 2, 3]
+const { generateLine } = require('./generateLine.js')
+const l1 = generateLine([1, 2, 3, 3, 2, 1])
 var removeDuplicateNodes = function(head) {
-  
+  var obj = {}
+  var prev = null
+  var current = head
+  while(current !== null) {
+    if (obj[current.val]) {
+      prev.next = current.next;
+      current = prev.next;
+    } else {
+      obj[current.val] = true
+      prev = current
+      current = current.next
+    }
+  }
+  return head
 };
+
+removeDuplicateNodes(l1)
