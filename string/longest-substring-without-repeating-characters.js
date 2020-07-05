1a820d9afb3ead39633804d081fdbd6fa42ae290
liuxing95 <1358974006@qq.com>
2020-07-05 21:56:31 +0800

    无重复字符的最长子串

---
+++ string/longest-substring-without-repeating-characters.js
@@ -0,0 +1,37 @@
+// 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+
+// 示例 1:
+
+// 输入: "abcabcbb"
+// 输出: 3 
+// 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+// 示例 2:
+
+// 输入: "bbbbb"
+// 输出: 1
+// 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+// 示例 3:
+
+// 输入: "pwwkew"
+// 输出: 3
+// 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+//      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+  let map=new Map();
+   let res=0
+   for(let i=0,j=0;j<s.length;j++){
+       if(map.has(s[j])){
+           i=Math.max(map.get(s[j])+1,i)
+       }
+       map.set(s[j],j);
+       res=Math.max(res,j-i+1);
+   }
+   return res
+};
+
+lengthOfLongestSubstring('pwwkew')
