5112a05f127a0ffd7c88a07c9e44f5bedc6ae626
liuxing95 <1358974006@qq.com>
2020-07-15 00:59:52 +0800

    最长回文子串 中心扩展法

--- string/longest-palindromic-substring.js
+++ string/longest-palindromic-substring.js
@@ -15,28 +15,59 @@
  * @return {string}
  */
 // 判断是否是回文串
-var isPalindrome = function(s) {
-  return s === s.split('').reverse().join('')
-}
-var longestPalindrome = function(s) {
-  if (!s) return ''
-  var palindrome = ''
-  var i = 0;
-  while(i < s.length) {
-    if (s.length - i < palindrome.length) break
-    var len = s.length
-    while( i < len) {
-      if (len - i < palindrome.length) break
-      if (isPalindrome(s.slice(i, len))) {
-        palindrome = (len - i) > palindrome.length ? s.slice(i, len) : palindrome
-        break;
-      }
-      len--
-    }
-    i++;
-  }
-  return palindrome
-};
+// 暴力破解法
+// var isPalindrome = function(s) {
+//   return s === s.split('').reverse().join('')
+// }
+// var longestPalindrome = function(s) {
+//   if (!s) return ''
+//   var palindrome = ''
+//   var i = 0;
+//   while(i < s.length) {
+//     if (s.length - i < palindrome.length) break
+//     var len = s.length
+//     while( i < len) {
+//       if (len - i < palindrome.length) break
+//       if (isPalindrome(s.slice(i, len))) {
+//         palindrome = (len - i) > palindrome.length ? s.slice(i, len) : palindrome
+//         break;
+//       }
+//       len--
+//     }
+//     i++;
+//   }
+//   return palindrome
+// };
 
-console.log(longestPalindrome('babad'))
-console.log(longestPalindrome("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"))
+
+// 2. 中心扩展法
+var longestPalindrome = function(s) {
+  if (!s || s.length < 2) return s
+
+  let start = 0, end = 0, n = s.length;
+  // 中心扩展法
+  let centerExpend = (left, right) => {
+    while(left >= 0 && right < n && s[left] === s[right]) {
+      left--
+      right++
+    }
+
+    return right - left - 1
+  }
+
+  for (let i = 0; i < n; i++) {
+    let len1 = centerExpend(i,i);
+    let len2 = centerExpend(i, i + 1);
+    // 两种组合取最大回文串的长度
+    let maxLen = Math.max(len1,len2);
+    if(maxLen > end - start){
+      // 更新最大回文串的首尾字符索引
+      start = i - ((maxLen - 1) >> 1);
+      end = i + (maxLen >> 1);
+    }
+  }
+  return s.substring(start,end+1);
+}
+
+console.log(longestPalindrome('bb'))
+// console.log(longestPalindrome("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"))
